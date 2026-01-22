259974c6d99342f595c5c65a86c9315b65ed5a71
JinYeon-Park <a9526191@gmail.com>
2026-01-22 15:28:46 +0900

    T3PlayerInputState 구조체 추가

---
+++ Source/Desecration/Public/Player/T3PlayerInputState.h
@@ -0,0 +1,44 @@
+// T3PlayerInputState.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "T3PlayerInputState.generated.h"
+
+UENUM(BlueprintType)
+enum class EDodgeDirection : uint8
+{
+	Forward,
+	ForwardRight,
+	Right,
+	BackRight,
+	Back,
+	BackLeft,
+	Left,
+	ForwardLeft,
+	Neutral
+};
+
+USTRUCT(BlueprintType)
+struct DESECRATION_API FT3PlayerInputState
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerInputState")
+	bool bWantsToMove = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerInputState")
+	bool bWantsToDodge = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerInputState")
+	bool bIsLockOn = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerInputState")
+	bool bIsFalling = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerInputState")
+	float CurrentSpeed = 0.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerInputState")
+	float FallingSpeed = 0.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerInputState")
+	EDodgeDirection DodgeDirection = EDodgeDirection::Neutral;
+	
+};
