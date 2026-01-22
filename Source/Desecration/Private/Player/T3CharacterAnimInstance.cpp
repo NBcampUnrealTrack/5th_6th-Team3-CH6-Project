f90fd94bf1296927835170bcbdd7cde8e4bc5df0
JinYeon-Park <a9526191@gmail.com>
2026-01-22 19:40:47 +0900

    ABP에 작성한 애님 인스턴스 상속

--- Source/Desecration/Private/Player/T3CharacterAnimInstance.cpp
+++ Source/Desecration/Private/Player/T3CharacterAnimInstance.cpp
@@ -1,5 +1,36 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// T3CharacterAnimInstance.cpp
 
 
 #include "Player/T3CharacterAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+void UT3CharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	OwnerCharacter = Cast<ACharacter>(TryGetPawnOwner());
+	
+	if (OwnerCharacter)
+	{
+		OwnerMovement = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UT3CharacterAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+	
+	if (!OwnerCharacter || !OwnerMovement)
+	{
+		return;
+	}
+	
+	float CurrentAccelerationSq = OwnerMovement->GetCurrentAcceleration().SizeSquared();
+	PlayerInputState.bWantsToMove = CurrentAccelerationSq > KINDA_SMALL_NUMBER;
+	
+	float CurrentGroundSpeedSq = OwnerCharacter->GetVelocity().SizeSquared2D();
+	
+	const float MoveThreshold = 3.0f;
+	PlayerInputState.bIsMoving = CurrentGroundSpeedSq > (MoveThreshold * MoveThreshold);
+}
--- Source/Desecration/Public/Player/T3CharacterAnimInstance.h
+++ Source/Desecration/Public/Player/T3CharacterAnimInstance.h
@@ -1,17 +1,32 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// T3CharacterAnimInstance.h
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "T3PlayerInputState.h"
 #include "T3CharacterAnimInstance.generated.h"
 
-/**
- * 
- */
+class UCharacterMovementComponent;
+
 UCLASS()
 class DESECRATION_API UT3CharacterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+	
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CharacterAnimation")
+	FT3PlayerInputState PlayerInputState;
+	
+protected:
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "CharacterAnimation")
+	TObjectPtr<ACharacter> OwnerCharacter;
+	
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "CharacterAnimation")
+	TObjectPtr<UCharacterMovementComponent> OwnerMovement;
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
 	
 };
