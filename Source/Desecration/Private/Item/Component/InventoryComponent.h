62f048acfca2c28fb786f56c699f8e6b6098aa16
ggyu-ik <ggye0723@gmail.com>
2026-01-22 13:03:49 +0900

    add 룬, 소모품 데이터테이블 추가 및 인벤토리 정의만

---
+++ Source/Desecration/Private/Item/Component/InventoryComponent.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/DataTable.h"
+#include "InventoryComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnInventoryUpdated);
+
+USTRUCT(BlueprintType)
+struct FInventorySlot
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Inventory Slot")
+	FName ItemID;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Inventory Slot")
+	int32 ItemStack = 0;
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class DESECRATION_API UInventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UInventoryComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Inventory")
+	TArray<FInventorySlot> Items;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Inventory")
+	int32 InventorySize = 10;
+	
+	UPROPERTY(BlueprintAssignable, Category = "Inventory")
+	FOnInventoryUpdated OnInventoryUpdated;
+	
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	void AddItem(FName ItemName);
+	
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	void UseItem(int32 SlotIndex);
+	
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	void DropItem(int32 SlotIndex);
+};
