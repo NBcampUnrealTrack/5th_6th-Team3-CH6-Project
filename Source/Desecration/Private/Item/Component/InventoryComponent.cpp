3bb798d724865dc0c901689cc20b3105b8285546
ggyu-ik <ggye0723@gmail.com>
2026-01-22 14:40:32 +0900

    move 인벤토리컴포넌트 헤더파일 public으로 옮김

--- Source/Desecration/Private/Item/Component/InventoryComponent.cpp
+++ Source/Desecration/Private/Item/Component/InventoryComponent.cpp
@@ -1,15 +1,11 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "InventoryComponent.h"
-
+﻿#include "Public/Item/Component/InventoryComponent.h"
 
 // Sets default values for this component's properties
 UInventoryComponent::UInventoryComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
@@ -24,13 +20,14 @@ void UInventoryComponent::BeginPlay()
 	
 }
 
-
-// Called every frame
-void UInventoryComponent::TickComponent(float DeltaTime, ELevelTick TickType,
-                                        FActorComponentTickFunction* ThisTickFunction)
+void UInventoryComponent::AddItem(FName ItemName)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
 
+void UInventoryComponent::UseItem(int32 SlotIndex)
+{
+}
+
+void UInventoryComponent::DropItem(int32 SlotIndex)
+{
+}
