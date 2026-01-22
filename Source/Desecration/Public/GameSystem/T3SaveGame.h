e1a4a9077f8af1532c2a126cdf4f882e81b42925
TheParkYB <park94128@naver.com>
2026-01-22 17:57:57 +0900

    [Add] 게임 저장 클래스 파일

---
+++ Source/Desecration/Public/GameSystem/T3SaveGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "T3SaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DESECRATION_API UT3SaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
