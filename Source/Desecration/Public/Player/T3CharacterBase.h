a417a7d73e7a2305f6a098566738934ac73957e1
tls9603-web <tls9603@gmail.com>
2026-01-23 17:43:55 +0900

    .

--- Source/Desecration/Public/Player/T3CharacterBase.h
+++ Source/Desecration/Public/Player/T3CharacterBase.h
@@ -5,13 +5,15 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "T3PlayerInputState.h"
-#include "InputActionValue.h"
 #include "T3CharacterBase.generated.h"
+
 
 class USpringArmComponent;
 class UCameraComponent;
 class UT3CombatComponent;
 class UDataTable;
+class UT3InventoryComponent; 
+class UT3ItemUseComponent;
 
 UCLASS()
 class DESECRATION_API AT3CharacterBase : public ACharacter
@@ -26,7 +28,6 @@ TObjectPtr <UDataTable> ItemDataTable;
 
 protected:
 	virtual void BeginPlay() override;
-	virtual void Tick( float DeltaTime ) override;
 
 
 protected:
@@ -40,20 +41,18 @@ protected:
 	TObjectPtr<UT3CombatComponent> CombatComponent;
 
 public:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "PlayerInputState")
-	FT3PlayerInputState PlayerInputState;
-	
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
-	FORCEINLINE UT3CombatComponent* GetCombatComponent() const { return CombatComponent; }
+	FORCEINLINE TObjectPtr <USpringArmComponent> GetCameraBoom() const { return CameraBoom; }
+	FORCEINLINE TObjectPtr <UCameraComponent> GetFollowCamera() const { return FollowCamera; }
+	FORCEINLINE TObjectPtr <UT3CombatComponent> GetCombatComponent() const { return CombatComponent; }
 
 	void Move(const FVector2D& Value);
 	void Look(const FVector2D& Value);
-	void Roll(const FInputActionValue& Value);
-	UFUNCTION(BlueprintImplementableEvent)
-	void OnRollTriggered();
-	UFUNCTION(BlueprintImplementableEvent)
-	void OnAttack();
+
+	// 아이템 관련 컴포넌트
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
+	TObjectPtr<UT3InventoryComponent> InventoryComponent; 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
+	TObjectPtr<UT3ItemUseComponent> ItemUseComponent;
 
 
 
@@ -145,13 +144,14 @@ protected:
 	float StaminaRegenInterval = 0.1f;
 	FTimerHandle StaminaRegenTimerHandle;
 	
-	UFUNCTION(BlueprintPure)
-	ERollDirection GetRollDirection(float Angle) const;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "PlayerInputState")
+	FT3PlayerInputState PlayerInputState;
 
 private:
 	// 내부 수치 계산 및 제한(Clamp)용 로직
 	void AddHP(float Amount);
 	void AddMP(float Amount);
 	void AddStamina(float Amount);
+
 
 };
