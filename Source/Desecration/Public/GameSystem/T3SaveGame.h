5d6b21ef3c68557462ea2050feb7e14c15fc0aed
TheParkYB <park94128@naver.com>
2026-01-23 17:13:02 +0900

    [Docs] 불필요 주석 제거

--- Source/Desecration/Public/GameSystem/T3SaveGame.h
+++ Source/Desecration/Public/GameSystem/T3SaveGame.h
@@ -1,14 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/SaveGame.h"
 #include "T3SaveGame.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class DESECRATION_API UT3SaveGame : public USaveGame
 {
