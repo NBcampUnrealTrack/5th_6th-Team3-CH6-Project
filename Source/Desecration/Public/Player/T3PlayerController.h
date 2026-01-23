88921a9be5cf6b7025139d23b89d52a69781e70a
ggyu-ik <ggye0723@gmail.com>
2026-01-23 17:59:26 +0900

    update 캐릭터에 인벤토리, 아이템사용 컴포넌트 부착 및 인벤토리 매핑, 바인딩

--- Source/Desecration/Public/Player/T3PlayerController.h
+++ Source/Desecration/Public/Player/T3PlayerController.h
@@ -9,6 +9,7 @@
 
 class UInputMappingContext;
 class UInputAction;
+class UUserWidget;
 
 UCLASS()
 class DESECRATION_API AT3PlayerController : public APlayerController
@@ -43,6 +44,15 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Input")
 	TObjectPtr<UInputAction> AttackAction;
 
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> InventoryAction;
+
+	UPROPERTY(EditDefaultsOnly, Category="UI")
+	TSubclassOf<UUserWidget> MainInventoryWidgetClass;
+	
+	UPROPERTY(BlueprintReadOnly, Category="UI")
+	TObjectPtr<UUserWidget> MainInventoryWidget;
+	
 private:
 	void Input_Move(const FInputActionValue& Value);
 	void Input_Look(const FInputActionValue& Value);
@@ -55,4 +65,5 @@ private:
 
 	void Input_Attack(const FInputActionValue& Value);
 
+	void ToggleInventoryInput();
 };
