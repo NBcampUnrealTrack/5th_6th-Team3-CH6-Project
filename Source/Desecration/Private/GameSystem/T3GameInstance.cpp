dfc8ace3d1b75f632091e8ea9f3952fbc8d94a84
TheParkYB <park94128@naver.com>
2026-01-26 13:02:43 +0900

    [Mod[ 마우스 감도의 기본값

--- Source/Desecration/Private/GameSystem/T3GameInstance.cpp
+++ Source/Desecration/Private/GameSystem/T3GameInstance.cpp
@@ -50,6 +50,8 @@ void UT3GameInstance::MakeFirstSettings()
 	//효과음, 배경음 모두 0.8을 기본으로
 	CurrentSettings->SoundEffectsVolume = 0.8f;
 	CurrentSettings->BackgroundVolume = 0.8f;
+	//마우스 감도는 0.5를 기본으로
+	CurrentSettings->MouseSensitivity = 0.5f;
 }
 
 
