456283e5f2212aa8526fc1d2d62e89ddacaf7a50
TheParkYB <park94128@naver.com>
2026-01-29 21:10:37 +0900

    [Mod] 타이틀 화면의 위젯에 사용할 변수 추가, 초기화 지정

--- Source/Desecration/Private/UI/TitleLevelWidget.cpp
+++ Source/Desecration/Private/UI/TitleLevelWidget.cpp
@@ -1,2 +1,65 @@
 #include "UI/TitleLevelWidget.h"
 
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "GameSystem/T3GameInstance.h"
+#include "GameSystem/T3TitleGameMode.h"
+
+void UTitleLevelWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	//게임 모드
+	TitleGameMode = Cast<AT3TitleGameMode>(GetWorld()->GetAuthGameMode());
+	if (!TitleGameMode)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s : TitleGameMode is NULL"), *GetNameSafe(this));
+		return;
+	}
+	
+	//게임 인스턴스
+	T3GameInstance = Cast<UT3GameInstance>(GetGameInstance());
+	if (!TitleGameMode)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s : T3GameInstance is NULL"), *GetNameSafe(this));
+		return;
+	}
+	
+	//저장된 게임
+	CurrentSaveGame = T3GameInstance->LoadGame();
+	
+	//버튼 바인딩
+	NewGameButton->OnClicked.AddDynamic(this, &ThisClass::OnClickNewGameButton);
+	SettingsButton->OnClicked.AddDynamic(this, &ThisClass::OnClickSettingsButton);
+	QuitButton->OnClicked.AddDynamic(this, &ThisClass::OnClickQuitButton);
+	//불러오기는 저장된 게임이 있을 때만 사용
+	if (CurrentSaveGame)
+	{
+		LoadButton->OnClicked.AddDynamic(this, &ThisClass::OnClickLoadButton);
+	}
+	else
+	{
+		LoadButton->SetIsEnabled(false);
+		LoadButtonText->SetIsEnabled(false);
+	}
+}
+
+void UTitleLevelWidget::OnClickNewGameButton()
+{
+	
+}
+
+void UTitleLevelWidget::OnClickLoadButton()
+{
+	
+}
+
+void UTitleLevelWidget::OnClickSettingsButton()
+{
+	
+}
+
+void UTitleLevelWidget::OnClickQuitButton()
+{
+	
+}
--- Source/Desecration/Public/UI/TitleLevelWidget.h
+++ Source/Desecration/Public/UI/TitleLevelWidget.h
@@ -4,9 +4,66 @@
 #include "Blueprint/UserWidget.h"
 #include "TitleLevelWidget.generated.h"
 
+class UTextBlock;
+class UT3SaveGame;
+class UT3GameInstance;
+class AT3TitleGameMode;
+class UButton;
+
 UCLASS()
 class DESECRATION_API UTitleLevelWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeConstruct() override;
+	
+private:
+	//새로하기
+	UFUNCTION()
+	void OnClickNewGameButton();
+	
+	//이어하기
+	UFUNCTION()
+	void OnClickLoadButton();
+	
+	//설정
+	UFUNCTION()
+	void OnClickSettingsButton();
+	
+	//나가기
+	UFUNCTION()
+	void OnClickQuitButton();
+	
+	//새로하기 버튼
+	UPROPERTY(meta = (AllowPrivateAccess = true, BindWidget))
+	TObjectPtr<UButton> NewGameButton;
+	
+	//이어하기 버튼
+	UPROPERTY(meta = (AllowPrivateAccess = true, BindWidget))
+	TObjectPtr<UButton> LoadButton;
+	
+	//이어하기 버튼의 텍스트
+	UPROPERTY(meta = (AllowPrivateAccess = true, BindWidget))
+	TObjectPtr<UTextBlock> LoadButtonText;
+	
+	//설정 버튼
+	UPROPERTY(meta = (AllowPrivateAccess = true, BindWidget))
+	TObjectPtr<UButton> SettingsButton;
+	
+	//나가기 버튼
+	UPROPERTY(meta = (AllowPrivateAccess = true, BindWidget))
+	TObjectPtr<UButton> QuitButton;
+	
+	//게임 모드
+	UPROPERTY()
+	TObjectPtr<AT3TitleGameMode> TitleGameMode;
+	
+	//게임 인스턴스
+	UPROPERTY()
+	TObjectPtr<UT3GameInstance> T3GameInstance;
+	
+	//저장된 게임
+	UPROPERTY()
+	TObjectPtr<UT3SaveGame> CurrentSaveGame;
 };
