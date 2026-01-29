ec0ed1e16b215217398b8915e51956f26ee6286e
TheParkYB <park94128@naver.com>
2026-01-29 12:13:20 +0900

    [Mod] 시작 화면의 게임 모드에 함수 추가

--- Source/Desecration/Private/GameSystem/T3TitleGameMode.cpp
+++ Source/Desecration/Private/GameSystem/T3TitleGameMode.cpp
@@ -1,5 +1,20 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "GameSystem/T3TitleGameMode.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void AT3TitleGameMode::MoveToSelectClassLevel()
+{
+	//TODO : 클래스 선택 맵으로 이동
+	UGameplayStatics::OpenLevel(GetWorld(), CLASS_LEVEL_NAME);
+}
+
+void AT3TitleGameMode::MoveToLastSavedLevel()
+{
+	//TODO : 마지막 저장 장소인 맵으로 이동
+}
+
+void AT3TitleGameMode::QuitGame() const
+{
+	UKismetSystemLibrary::QuitGame(GetWorld(), GetWorld()->GetFirstPlayerController(), EQuitPreference::Quit, false);
+}
