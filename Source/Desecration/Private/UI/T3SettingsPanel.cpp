b05166c32fc97d6f1e47500efc197cc15fc571f6
TheParkYB <park94128@naver.com>
2026-01-30 21:53:31 +0900

    [Mod] 설정 패널에 사용할 변수와 함수

--- Source/Desecration/Private/UI/T3SettingsPanel.cpp
+++ Source/Desecration/Private/UI/T3SettingsPanel.cpp
@@ -1,2 +1,85 @@
 #include "UI/T3SettingsPanel.h"
 
+#include "Components/Button.h"
+#include "Components/ComboBoxString.h"
+#include "Components/Slider.h"
+#include "GameSystem/T3GameInstance.h"
+#include "GameSystem/T3TitleGameMode.h"
+#include "Player/T3TitlePlayerController.h"
+
+void UT3SettingsPanel::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	//게임 모드
+	TitleGameMode = Cast<AT3TitleGameMode>(GetWorld()->GetAuthGameMode());
+	if (!TitleGameMode)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s : TitleGameMode is NULL"), *GetNameSafe(this));
+		return;
+	}
+	
+	//게임 인스턴스
+	T3GameInstance = Cast<UT3GameInstance>(GetGameInstance());
+	if (!TitleGameMode)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s : T3GameInstance is NULL"), *GetNameSafe(this));
+		return;
+	}
+	
+	//플레이어 컨트롤러
+	TitlePlayerController = Cast<AT3TitlePlayerController>(GetOwningPlayer());
+	if (!TitlePlayerController)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s : TitlePlayerController is NULL"), *GetNameSafe(this));
+		return;
+	}
+	
+	//슬라이더 바인딩
+	BGMSlider->OnValueChanged.AddDynamic(this, &ThisClass::WhileMovingBGMSlider);
+	SESlider->OnValueChanged.AddDynamic(this, &ThisClass::WhileMovingSESlider);
+	MouseSensitivitySlider->OnValueChanged.AddDynamic(this, &ThisClass::WhileMovingMouseSensitivitySlider);
+	
+	//콤보박스 바인딩
+	ResolutionComboBox->OnSelectionChanged.AddDynamic(this, &ThisClass::OnSelectResolutionComboBox);
+	ScreenModeComboBox->OnSelectionChanged.AddDynamic(this, &ThisClass::OnSelectScreenModeComboBox);
+	GraphicQualityComboBox->OnSelectionChanged.AddDynamic(this, &ThisClass::OnSelectGraphicQualityComboBox);
+	
+	//버튼 바인딩
+	ConfirmButton->OnClicked.AddDynamic(this, &ThisClass::OnClickConfirmButton);
+}
+
+void UT3SettingsPanel::WhileMovingBGMSlider(float value)
+{
+	
+}
+
+void UT3SettingsPanel::WhileMovingSESlider(float value)
+{
+	
+}
+
+void UT3SettingsPanel::WhileMovingMouseSensitivitySlider(float value)
+{
+	
+}
+
+void UT3SettingsPanel::OnSelectResolutionComboBox(FString SelectedItem, ESelectInfo::Type SelectionType)
+{
+	
+}
+
+void UT3SettingsPanel::OnSelectScreenModeComboBox(FString SelectedItem, ESelectInfo::Type SelectionType)
+{
+	
+}
+
+void UT3SettingsPanel::OnSelectGraphicQualityComboBox(FString SelectedItem, ESelectInfo::Type SelectionType)
+{
+	
+}
+
+void UT3SettingsPanel::OnClickConfirmButton()
+{
+	
+}
--- Source/Desecration/Public/UI/T3SettingsPanel.h
+++ Source/Desecration/Public/UI/T3SettingsPanel.h
@@ -4,9 +4,87 @@
 #include "Blueprint/UserWidget.h"
 #include "T3SettingsPanel.generated.h"
 
+class AT3TitlePlayerController;
+class UT3GameInstance;
+class AT3TitleGameMode;
+class UButton;
+class UComboBoxString;
+class USlider;
+
 UCLASS()
 class DESECRATION_API UT3SettingsPanel : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeConstruct() override;
+	
+private:
+	//배경음 슬라이더
+	UFUNCTION()
+	void WhileMovingBGMSlider(float value);
+	
+	//효과음 슬라이더
+	UFUNCTION()
+	void WhileMovingSESlider(float value);
+	
+	//마우스 감도 슬라이더
+	UFUNCTION()
+	void WhileMovingMouseSensitivitySlider(float value);
+	
+	//해상도 콤보박스
+	UFUNCTION()
+	void OnSelectResolutionComboBox(FString SelectedItem, ESelectInfo::Type SelectionType);
+	
+	//화면 모드 콤보박스
+	UFUNCTION()
+	void OnSelectScreenModeComboBox(FString SelectedItem, ESelectInfo::Type SelectionType);
+	
+	//그래픽 퀄리티 콤보박스
+	UFUNCTION()
+	void OnSelectGraphicQualityComboBox(FString SelectedItem, ESelectInfo::Type SelectionType);
+	
+	//확인 버튼
+	UFUNCTION()
+	void OnClickConfirmButton();
+	
+	//배경음 슬라이더
+	UPROPERTY(meta = (AllowPrivateAccess = true, BindWidget))
+	TObjectPtr<USlider> BGMSlider;
+	
+	//효과음 슬라이더
+	UPROPERTY(meta = (AllowPrivateAccess = true, BindWidget))
+	TObjectPtr<USlider> SESlider;
+	
+	//마우스 감도 슬라이더
+	UPROPERTY(meta = (AllowPrivateAccess = true, BindWidget))
+	TObjectPtr<USlider> MouseSensitivitySlider;
+	
+	//해상도 콤보박스
+	UPROPERTY(meta = (AllowPrivateAccess = true, BindWidget))
+	TObjectPtr<UComboBoxString> ResolutionComboBox;
+	
+	//화면 모드 콤보박스
+	UPROPERTY(meta = (AllowPrivateAccess = true, BindWidget))
+	TObjectPtr<UComboBoxString> ScreenModeComboBox;
+	
+	//그래픽 퀄리티 콤보박스
+	UPROPERTY(meta = (AllowPrivateAccess = true, BindWidget))
+	TObjectPtr<UComboBoxString> GraphicQualityComboBox;
+	
+	//확인 버튼
+	UPROPERTY(meta = (AllowPrivateAccess = true, BindWidget))
+	TObjectPtr<UButton> ConfirmButton;
+	
+	//게임 모드
+	UPROPERTY()
+	TObjectPtr<AT3TitleGameMode> TitleGameMode;
+	
+	//게임 인스턴스
+	UPROPERTY()
+	TObjectPtr<UT3GameInstance> T3GameInstance;
+	
+	//풀레이어 컨트롤러
+	UPROPERTY()
+	TObjectPtr<AT3TitlePlayerController> TitlePlayerController;
 };
