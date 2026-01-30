53ff29d05a5d01e695ad6851addb1956336ace09
Jaeuk No <127023656+NoJaeuk@users.noreply.github.com>
2026-01-30 18:58:30 +0900

    Fix 패키징 버그 수정

--- Source/Desecration/Public/Player/T3CharacterBase.h
+++ Source/Desecration/Public/Player/T3CharacterBase.h
@@ -117,7 +117,7 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stat")
 	float MaxStamina = 100.f;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stat")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stat")
 	float CurrentStamina;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stat")
@@ -172,7 +172,7 @@ public:
 
 	// Speed
 	UFUNCTION(BlueprintCallable, Category = "Stat")
-	FORCEINLINE float GetMoveSpeed() const;
+	float GetMoveSpeed() const;
 
 	UFUNCTION(BlueprintCallable, Category = "Stat")
 	void SetMoveSpeed(float NewSpeed);
