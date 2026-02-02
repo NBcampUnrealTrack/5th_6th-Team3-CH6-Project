dff9d84c7722f11d0347fca3c728f8eef1885070
YUYEONY <cybott2002@gmail.com>
2026-02-02 22:39:21 +0900

    [Update] 성채 벽 일부 프렉처 모드 사용하여 부서진 곳 만들기, 내성 입구 동상 배치
    [Update] 성채 벽 일부 프렉처 모드 사용하여 부서진 곳 만들기, 내성 입구 동상 배치

--- Source/Desecration/Public/GameSystem/GlobalEnums.h
+++ Source/Desecration/Public/GameSystem/GlobalEnums.h
@@ -46,10 +46,7 @@ enum class ELevelName
 {
 	Title = 0 UMETA(DisplayName = "TitleLevel"),
 	SelectClass UMETA(DisplayName = "SelectClassLevel"),
-};
+	Town UMETA(DisplayName = "T3Town"),
+	Castle_1 = 0 UMETA(DisplayName = "T3Castle_1"),
 
-enum class T3Castle_1
-{
-	Title = 0 UMETA(DisplayName = "TitleLevel"),
-	SelectClass UMETA(DisplayName = "SelectClassLevel"),
 };
