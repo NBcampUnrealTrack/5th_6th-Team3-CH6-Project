ab85cea70a1c4a4b440932836900c8ca2eb0bbdc
TheParkYB <park94128@naver.com>
2026-02-04 01:33:07 +0900

    [Add] 클래스 선택 레벨의 플레이어 컨트롤러 코드 및 블루프린트

---
+++ Source/Desecration/Private/Player/T3SelectClassPlayerController.cpp
@@ -0,0 +1,50 @@
+#include "Player/T3SelectClassPlayerController.h"
+
+#include "GameSystem/T3GameInstance.h"
+#include "GameSystem/T3SelectClassGameMode.h"
+#include "UI/T3SelectClassWidget.h"
+
+void AT3SelectClassPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	//마우스 사용
+	bShowMouseCursor = true;
+	const FInputModeUIOnly InputModeUIOnly;
+	SetInputMode(InputModeUIOnly);
+	
+	//게임 모드
+	SelectClassGameMode = Cast<AT3SelectClassGameMode>(GetWorld()->GetAuthGameMode());
+	if (!SelectClassGameMode)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s : TitleGameMode is NULL"), *GetNameSafe(this));
+		return;
+	}
+	
+	//게임 인스턴스
+	T3GameInstance = Cast<UT3GameInstance>(GetGameInstance());
+	if (!T3GameInstance)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s : T3GameInstance is NULL"), *GetNameSafe(this));
+		return;
+	}
+	
+	//위젯 생성
+	SelectClassWidgetInstance = CreateWidget<UT3SelectClassWidget>(this, SelectClassWidgetClass);
+	if (!SelectClassWidgetInstance)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s : Failed to create TitleLevelWidget"), *GetNameSafe(this));
+		return;
+	}
+	SelectClassWidgetInstance->AddToViewport();
+}
+
+void AT3SelectClassPlayerController::TutorialStart()
+{
+	
+}
+
+void AT3SelectClassPlayerController::ReturnToTitleLevel()
+{
+	SelectClassGameMode->ReturnToTitleLevel();
+}
