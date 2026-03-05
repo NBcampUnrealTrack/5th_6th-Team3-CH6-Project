9da0fbef098182c77eb31737dd01559443297076
TheParkYB <park94128@naver.com>
2026-03-05 21:58:32 +0900

    [Mod] 팝업 메뉴의 설정 패널 추가 및 설정 버튼 작동

--- Source/Desecration/Private/UI/T3PopUpMenu.cpp
+++ Source/Desecration/Private/UI/T3PopUpMenu.cpp
@@ -5,6 +5,7 @@
 #include "GameSystem/T3GameMode.h"
 #include "Player/T3PlayerController.h"
 #include "UI/T3ConfirmPanel.h"
+#include "UI/T3SettingsPanel.h"
 
 void UT3PopUpMenu::NativeConstruct()
 {
@@ -37,10 +38,22 @@ void UT3PopUpMenu::NativeConstruct()
 	//버튼 바인딩
 	ResumeButton->OnClicked.AddDynamic(this, &ThisClass::OnClickResumeButton);
 	LoadButton->OnClicked.AddDynamic(this, &ThisClass::OnClickLoadButton);
-	SettingsButton->OnClicked.AddDynamic(this, &ThisClass::OnClickSettingsButton);
 	TitleButton->OnClicked.AddDynamic(this, &ThisClass::OnClickTitleButton);
+	//설정 버튼은 마을에서만 동작
+	if (T3GameInstance->GetCurrentLevel() == ELevelName::Town)
+	{
+		SettingsButton->OnClicked.AddDynamic(this, &ThisClass::OnClickSettingsButton);
+	}
+	else
+	{
+		SettingsButton->SetIsEnabled(false);
+	}
+	
+	//설정 패널을 닫을 때 메뉴 복구하기
+	SettingsPanel->OnClosePanel.BindUObject(this, &ThisClass::OnCloseSettingsPanel);
 	
 	//위젯은 닫힌 상태로 시작
+	SettingsPanel->SetVisibility(ESlateVisibility::Collapsed);
 	ConfirmPanel->SetVisibility(ESlateVisibility::Collapsed);
 	SetVisibility(ESlateVisibility::Collapsed);
 }
@@ -75,13 +88,19 @@ void UT3PopUpMenu::OnClickLoadButton()
 
 void UT3PopUpMenu::OnClickSettingsButton()
 {
-	
+	MenuBorder->SetVisibility(ESlateVisibility::Collapsed);
+	SettingsPanel->OpenSettingsPanel();
 }
 
 void UT3PopUpMenu::OnClickTitleButton()
 {
 	ConfirmPanel->ShowConfirmPanel(CheckGotoTitle);
 	ConfirmPanel->OnClickConfirmButtonAction.AddDynamic(this, &ThisClass::GotoTitle);
+}
+
+void UT3PopUpMenu::OnCloseSettingsPanel()
+{
+	MenuBorder->SetVisibility(ESlateVisibility::Visible);
 }
 
 void UT3PopUpMenu::GotoTitle()
