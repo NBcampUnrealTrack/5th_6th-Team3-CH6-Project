25cd2e1b368d80806ec42ec1b36e02ee653e9911
ggyu-ik <ggye0723@gmail.com>
2026-03-16 01:33:36 +0900

    update 강타, 처형 룬 보스에 적용 안되는거 수정중(미완성)

--- Source/Desecration/Private/Player/T3CombatComponent.cpp
+++ Source/Desecration/Private/Player/T3CombatComponent.cpp
@@ -1,6 +1,8 @@
 ﻿// T3CombatComponent.cpp
 
 #include "Player/T3CombatComponent.h"
+
+#include "Desecration.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/PlayerController.h"
 #include "Kismet/KismetMathLibrary.h"
@@ -768,8 +770,25 @@ void UT3CombatComponent::RequestAttackDamage(AActor* TargetActor, float DamageAm
 	if (HitBoss)
 	{
 		// GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Red, TEXT("Hit Boss!"));
-		HitBoss->Damage(DamageAmount, InStunAmount);  // 테스트용 스턴 20
+		//HitBoss->Damage(DamageAmount, InStunAmount);  // 테스트용 스턴 20
 		// HitBoss->Damage(CurrentAttackDamage, StunAmount);
+		
+		float ActualDamage = DamageAmount;
+		
+		if (OwnerChar->GetSmiteThreshold() > 0 && OwnerChar->GetSmiteCounter() >= OwnerChar->GetSmiteThreshold())
+		{
+			ActualDamage = DamageAmount * OwnerChar->GetSmiteMultiplier();
+			
+			OwnerChar->SetSmiteCounter(0);
+		}
+		else
+		{
+			OwnerChar->IncrementSmiteCounter();
+		}
+
+		HitBoss->Damage(ActualDamage, InStunAmount);
+		
+		OwnerChar->OnDamageDealt.Broadcast(TargetActor, DamageAmount);
 	}
 	
 	// TakeDamage 호출 시 커스텀 이벤트 구조체를 전달
@@ -781,20 +800,18 @@ void UT3CombatComponent::RequestAttackDamage(AActor* TargetActor, float DamageAm
 		{
 			ActualDamage = DamageAmount * OwnerChar->GetSmiteMultiplier();
 			
-			TargetActor->TakeDamage(ActualDamage, T3DamageEvent, OwnerPC, OwnerChar);
-			
 			OwnerChar->SetSmiteCounter(0);
 		}
 		else
 		{
 			OwnerChar->IncrementSmiteCounter();
-			
-			TargetActor->TakeDamage(ActualDamage, T3DamageEvent, OwnerPC, OwnerChar);
 		}
-
+		
+		TargetActor->TakeDamage(ActualDamage, T3DamageEvent, OwnerPC, OwnerChar);
+		
 		OwnerChar->OnDamageDealt.Broadcast(TargetActor, ActualDamage);
 		
-		UE_LOG(LogTemp, Warning, TEXT("현재 공격 횟수 : %d"), OwnerChar->GetSmiteCounter());
+		UE_LOG(LogItem, Warning, TEXT("현재 공격 횟수 : %d"), OwnerChar->GetSmiteCounter());
 	}
 	else if (AIChar) // OwnerChar가 아닐 때만 AIChar로 실행
 	{
