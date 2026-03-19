0412f906eb8b6c069e84f957165d68fc13ca5f15
ggyu-ik <ggye0723@gmail.com>
2026-03-19 15:48:29 +0900

    update 팔라딘 룬 등급별 수치 적용 구현 및 레젠더리 시 패시브 강화 로직 구현

--- Source/Desecration/Private/Item/Rune/T3PaladinRune.cpp
+++ Source/Desecration/Private/Item/Rune/T3PaladinRune.cpp
@@ -1,6 +1,8 @@
 #include "Item/Rune/T3PaladinRune.h"
 
+#include "Equipment/T3EquipmentTypes.h"
 #include "Player/T3CombatComponent.h"
+#include "Player/Paladin/T3Paladin_SkillComponent.h"
 
 void UT3PaladinRune::OnSocketed_Implementation(AT3CharacterBase* OwnerChar)
 {
@@ -9,9 +11,23 @@ void UT3PaladinRune::OnSocketed_Implementation(AT3CharacterBase* OwnerChar)
 		return;
 	}
 	
-	OriginalHolyGaugeChargeAmount = OwnerChar->GetCombatComponent()->GetHolyGaugeChargeAmount();
+	UT3CombatComponent* Combat = OwnerChar->GetCombatComponent();
 	
-	OwnerChar->GetCombatComponent()->SetHolyGaugeChargeAmount(OriginalHolyGaugeChargeAmount * ValueByGrade);
+	OriginalHolyGaugeChargeAmount = Combat->GetHolyGaugeChargeAmount();
+	
+	Combat->SetHolyGaugeChargeAmount(OriginalHolyGaugeChargeAmount * (1.0f + ValueByGrade / 100.f));
+	
+	if (bIsLegendary)
+	{
+		UT3Paladin_SkillComponent* PaladinSkill = Cast<UT3Paladin_SkillComponent>(Combat->GetSkillComponent());
+		
+		if (IsValid(PaladinSkill))
+		{
+			OriginalHolyModeAttackSpeedMultiplier = PaladinSkill->GetHolyModeAttackSpeedMultiplier();
+			
+			PaladinSkill->SetHolyModeAttackSpeedMultiplier(1 + LegendaryAttackSpeedBonusPercent / 100.0f);
+		}
+	}
 }
 
 void UT3PaladinRune::OnUnsocketed_Implementation(AT3CharacterBase* OwnerChar)
@@ -22,4 +38,42 @@ void UT3PaladinRune::OnUnsocketed_Implementation(AT3CharacterBase* OwnerChar)
 	}
 	
 	OwnerChar->GetCombatComponent()->SetHolyGaugeChargeAmount(OriginalHolyGaugeChargeAmount);
+	
+	if (bIsLegendary)
+	{
+		UT3Paladin_SkillComponent* PaladinSkill = Cast<UT3Paladin_SkillComponent>(
+		OwnerChar->GetCombatComponent()->GetSkillComponent());
+	
+		if (IsValid(PaladinSkill))
+		{
+			PaladinSkill->SetHolyModeAttackSpeedMultiplier(OriginalHolyModeAttackSpeedMultiplier);
+		}
+	}
+}
+
+void UT3PaladinRune::SetGrade(ET3RuneGrade InGrade)
+{
+	switch (InGrade)
+	{
+	case ET3RuneGrade::Normal:
+		
+		ValueByGrade = HolyGaugeChargeBonusPercentNormal;
+		bIsLegendary = false;
+		break;
+		
+	case ET3RuneGrade::Epic:
+		
+		ValueByGrade = HolyGaugeChargeBonusPercentEpic;
+		bIsLegendary = false;
+		break;
+		
+	case ET3RuneGrade::Legendary:
+		
+		ValueByGrade = HolyGaugeChargeBonusPercentLegendary;
+		bIsLegendary = true;
+		break;
+		
+	default:
+		break;
+	}
 }
