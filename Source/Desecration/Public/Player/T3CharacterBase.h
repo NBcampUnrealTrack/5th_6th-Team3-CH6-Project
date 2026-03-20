25d0ab298b0235bb5bf8d624a956316c5a574ead
JinYeon-Park <a9526191@gmail.com>
2026-03-20 12:09:21 +0900

    상자 열때 애니메이션 재생하게끔 수정

--- Source/Desecration/Public/Player/T3CharacterBase.h
+++ Source/Desecration/Public/Player/T3CharacterBase.h
@@ -384,6 +384,9 @@ public:
 	
 	UFUNCTION(BlueprintImplementableEvent, Category = "CameraShake")
 	void OnCameraShake();
+	
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void OnOpenBox();
 
 
 	// ==== 캐릭터 스탯
