83e7c2db0f7c70483b50cbbe94b5c45018d0616d
ggyu-ik <ggye0723@gmail.com>
2026-03-26 15:36:12 +0900

    update 룬 합성 슬롯에 드래그 앤 드롭으로 넣고 빼는 로직 구현중(미완성)

--- Source/Desecration/Private/UI/Rune/T3SynthesisSlotWidget.cpp
+++ Source/Desecration/Private/UI/Rune/T3SynthesisSlotWidget.cpp
@@ -1,2 +1,66 @@
 #include "UI/Rune/T3SynthesisSlotWidget.h"
+#include "Components/Image.h"
+#include "Equipment/T3UpgradeStation.h"
+#include "UI/ItemDragDropOperation.h"
 
+void UT3SynthesisSlotWidget::SetRuneIcon(UTexture2D* Icon)
+{
+	if (!Img_RuneIcon)
+	{
+		return;
+	}
+
+	Img_RuneIcon->SetBrushFromTexture(Icon);
+	Img_RuneIcon->SetVisibility(ESlateVisibility::Visible);
+}
+
+void UT3SynthesisSlotWidget::ClearRuneIcon()
+{
+	if (!Img_RuneIcon)
+	{
+		return;
+	}
+
+	Img_RuneIcon->SetVisibility(ESlateVisibility::Collapsed);
+}
+
+FReply UT3SynthesisSlotWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	if (InMouseEvent.GetEffectingButton() == EKeys::RightMouseButton)
+	{
+		if (UpgradeStation && SlotIndex >= 0)
+		{
+			UpgradeStation->RemoveRuneFromSynthesisSlot(SlotIndex);
+		}
+		return FReply::Handled();
+	}
+
+	return Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
+}
+
+bool UT3SynthesisSlotWidget::NativeOnDragOver(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation)
+{
+	UItemDragDropOperation* ItemDragOp = Cast<UItemDragDropOperation>(InOperation);
+	if (!IsValid(ItemDragOp) || ItemDragOp->bIsFromRuneSocket)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool UT3SynthesisSlotWidget::NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation)
+{
+	UItemDragDropOperation* ItemDragOp = Cast<UItemDragDropOperation>(InOperation);
+	if (!IsValid(ItemDragOp) || ItemDragOp->bIsFromRuneSocket)
+	{
+		return false;
+	}
+
+	if (!UpgradeStation || ItemDragOp->DraggedItemID == NAME_None)
+	{
+		return false;
+	}
+
+	return UpgradeStation->AddRuneToSynthesisSlot(ItemDragOp->DraggedItemID);
+}
--- Source/Desecration/Public/UI/Rune/T3SynthesisSlotWidget.h
+++ Source/Desecration/Public/UI/Rune/T3SynthesisSlotWidget.h
@@ -4,9 +4,32 @@
 #include "Blueprint/UserWidget.h"
 #include "T3SynthesisSlotWidget.generated.h"
 
+class AT3UpgradeStation;
+class UImage;
+
 UCLASS()
 class DESECRATION_API UT3SynthesisSlotWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite, Category = "Synthesis")
+	int32 SlotIndex = -1;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Synthesis")
+	TObjectPtr<AT3UpgradeStation> UpgradeStation;
+
+	void SetRuneIcon(UTexture2D* Icon);
 	
+	void ClearRuneIcon();
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Img_RuneIcon;
+
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+
+	virtual bool NativeOnDragOver(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) override;
+
+	virtual bool NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) override;
 };
