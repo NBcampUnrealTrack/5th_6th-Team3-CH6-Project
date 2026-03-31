9f400dc8f692883d770d71d8dc427fe5d683ce03
TheParkYB <park94128@naver.com>
2026-03-31 21:17:18 +0900

    [Fix] 사라진 초기 스탯 공격력 복구

--- Source/Desecration/Private/GameSystem/T3SaveGame.cpp
+++ Source/Desecration/Private/GameSystem/T3SaveGame.cpp
@@ -27,7 +27,7 @@ void UT3SaveGame::ResetGameData()
 	CurrentMana = MaxMana;
 	MaxStamina = 100.0f;
 	CurrentStamina = MaxStamina;
-	StatAttackPower = 0.0;
+	StatAttackPower = 5.0f;
 	WeaponAttackPower = 45.0f;
 	RuneAttackPower = 0.0f;
 	CriticalChance = 0.1f;
