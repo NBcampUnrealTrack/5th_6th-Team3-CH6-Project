ec128e043e68498adee1d2021ca278f03e3f1b88
TheParkYB <park94128@naver.com>
2026-04-14 09:37:52 +0900

    [Mod] 임시 변경 롤백

--- Source/Desecration/Private/UI/T3SettingsPanel.cpp
+++ Source/Desecration/Private/UI/T3SettingsPanel.cpp
@@ -146,8 +146,7 @@ void UT3SettingsPanel::OpenSettingsPanel()
 	}
 	
 	//1번째 위젯을 연다.
-	//TODO : 버그 수정 후 첫 번째(0)를 여는 것으로 수정
-	OnClickTabButton(1);
+	OnClickTabButton(0);
 	
 	//패널 활성화
 	SetVisibility(ESlateVisibility::Visible);
