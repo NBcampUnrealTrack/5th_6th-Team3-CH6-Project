e59f361b6a5b56bf54d5bbd386db62b529537e06
ggyu-ik <ggye0723@gmail.com>
2026-04-25 17:03:05 +0900

    update 천사 장신구 범위 인식을 SphereComponent 로직으로 변경

--- Source/Desecration/Public/Equipment/Accessory/T3AngelAccessoryEffect.h
+++ Source/Desecration/Public/Equipment/Accessory/T3AngelAccessoryEffect.h
@@ -2,20 +2,27 @@
 
 #include "CoreMinimal.h"
 #include "T3AccessoryEffectBase.h"
+#include "Components/SphereComponent.h"
 #include "T3AngelAccessoryEffect.generated.h"
 
 UCLASS()
 class DESECRATION_API UT3AngelAccessoryEffect : public UT3AccessoryEffectBase
 {
 	GENERATED_BODY()
-	
+
 public:
 	virtual void OnEquipped_Implementation(AT3CharacterBase* OwnerChar) override;
-	
+
 	virtual void OnUnequipped_Implementation(AT3CharacterBase* OwnerChar) override;
 
 private:
-	void ApplySlowAura();
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	void RestoreMonster(int32 Index);
 
 	UPROPERTY(EditDefaultsOnly)
 	float SlowRadius = 400.f;
@@ -30,12 +37,10 @@ private:
 	float AttackAnimSlowAmount = 0.7f;
 
 	UPROPERTY(EditDefaultsOnly)
-	float SlowInterval = 1.0f;
-
-	UPROPERTY(EditDefaultsOnly)
 	bool bShowDebugRadius = false;
 
-	FTimerHandle SlowTimerHandle;
+	UPROPERTY()
+	USphereComponent* AuraSphere = nullptr;
 
 	TArray<TWeakObjectPtr<AActor>> SlowedMonsters;
 
