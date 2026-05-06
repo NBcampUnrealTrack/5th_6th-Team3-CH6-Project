292fa1e3840aa79c91ad3f8cbc3e7ad4fbf19f87
JinYeon-Park <a9526191@gmail.com>
2026-05-06 15:57:49 +0900

    Build.cs 에 MotionTrajectory 추가

--- Source/Desecration/Public/Player/T3CharacterAnimInstance.h
+++ Source/Desecration/Public/Player/T3CharacterAnimInstance.h
@@ -1,17 +1,39 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// T3CharacterAnimInstance.h
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "GameplayTagContainer.h"
+#include "CharacterTrajectoryComponent.h"
 #include "T3CharacterAnimInstance.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class DESECRATION_API UT3CharacterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Locomotion|Tuning")
+	float SpeedThreshold = 20.0f;
+	
+	UPROPERTY(EditDefualtsOnly, Category = "Locomotion|Tuning")
+	float FutureSampleTime = 0.3f;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Locomotion|State")
+	FGameplayTag CurrentLocomotionState;
+	
+	UPROPERTY(Transient)
+	TObjectPtr<UCharacterTrajectoryComponent> TrajectoryComponent;
+	
+	float CurrentSpeed;
+	float FutureSpeed;
+	
+	void DetermineLocomotionState();
 	
 };
