6f5e11aca473244a864924d81cf61aa5bbee5259
JinYeon-Park <a9526191@gmail.com>
2026-05-06 15:33:37 +0900

    CharacterAnimInstance C++ Class 생

---
+++ Source/Desecration/Public/Player/T3CharacterAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "T3CharacterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DESECRATION_API UT3CharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
