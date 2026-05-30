7894d7e3c6cf0925cdbee5f03b362a0ae773bfeb
TheParkYB <park94128@naver.com>
2026-05-30 22:33:05 +0900

    [Mod] 게임 패드 관련 내용을 폐기함에 따라 관련 설정 제거

--- Source/Desecration/Private/UI/T3ControllerSettings.cpp
+++ Source/Desecration/Private/UI/T3ControllerSettings.cpp
@@ -7,15 +7,7 @@
 #include "UI/T3Slider.h"
 
 void UT3ControllerSettings::OnParentConstruct()
-{	
-	//컨트롤러 선택 콤보 박스
-	for (const FString Key : CONTROLLER_KEY_STRINGS)
-	{		
-		const FString OptionString = UT3GameInstance::GetStringFromTable(NAMESPACE_NAME, Key);
-		ControllerComboBox->AddOption(OptionString);
-	}
-	ControllerComboBox->OnSelectionChanged.AddDynamic(this, &UT3ControllerSettings::OnSelectionChangedControllerComboBox);
-	
+{
 	//수직 회전 반전
 	InvertVerticalCheckBox->OnCheckStateChanged.AddDynamic(this, &UT3ControllerSettings::OnCheckStateChangedInvertVerticalCheckBox);
 	
@@ -26,50 +18,13 @@ void UT3ControllerSettings::OnParentConstruct()
 void UT3ControllerSettings::InitializeSettingsPanel()
 {
 	const TObjectPtr<UT3SaveUserSettings> CurrentSettings = T3GameInstance->GetCurrentSettings();
-	if (LayoutTextures.IsValidIndex(CurrentSettings->UsingController))
-	{
-		ControlGuideImage->SetBrushFromTexture(LayoutTextures[CurrentSettings->UsingController]);
-	}
-	ControllerComboBox->SetSelectedIndex(CurrentSettings->UsingController);
 	InvertVerticalCheckBox->SetCheckedState(CurrentSettings->bInvertVertical ? ECheckBoxState::Checked : ECheckBoxState::Unchecked);
 	CameraSpeedSlider->SetValue(CurrentSettings->CameraSpeed);
-}
-
-void UT3ControllerSettings::ReinitializeByChangeLanguage()
-{
-	//콤보 박스 리셋
-	const int32 TempIndex = ControllerComboBox->GetSelectedIndex();
-	ControllerComboBox->ClearOptions();
-	for (const FString Key : CONTROLLER_KEY_STRINGS)
-	{		
-		const FString OptionString = UT3GameInstance::GetStringFromTable(NAMESPACE_NAME, Key);
-		ControllerComboBox->AddOption(OptionString);
-	}
-	ControllerComboBox->SetSelectedIndex(TempIndex);
 }
 
 void UT3ControllerSettings::SaveSettings()
 {
 	T3GameInstance->SaveUserSettings();
-}
-
-void UT3ControllerSettings::OnSelectionChangedControllerComboBox(FString SelectedItem, ESelectInfo::Type SelectionType)
-{
-	//코드에 의한 변경은 무시
-	if (SelectionType == ESelectInfo::Type::Direct)
-	{
-		return;
-	}
-
-	//컨트롤러 가이드 기준 변경
-	const int32 SelectedIndex = ControllerComboBox->GetSelectedIndex();
-	T3GameInstance->GetCurrentSettings()->UsingController = SelectedIndex;
-	
-	//설정 패널의 UI 갱신
-	if (LayoutTextures.IsValidIndex(SelectedIndex))
-	{
-		ControlGuideImage->SetBrushFromTexture(LayoutTextures[SelectedIndex]);
-	}
 }
 
 void UT3ControllerSettings::OnCheckStateChangedInvertVerticalCheckBox(bool bIsChecked)
